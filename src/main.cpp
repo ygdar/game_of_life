9cb846089b6c2d03648c27ace711c39dba070e85
zombi <zombi@zombi.zo>
2024-09-03 22:49:59 +0300

    color grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+#include <random>
+#include <vector>
+#include <cmath>
 #include <SFML/Graphics.hpp>
 
 int main()
@@ -10,8 +12,29 @@ int main()
     window.setFramerateLimit(144);
 
     sf::Color borderColor(170, 108, 57);
+
+    std::vector<sf::Color> openCellColors = {
+            sf::Color(102, 153, 153),
+            sf::Color(64, 127, 127),
+            sf::Color(34, 102, 102),
+            sf::Color(13, 77, 77),
+            sf::Color(0, 51, 51),
+    };
+
+    std::vector<sf::Color> closedCellColors = {
+            sf::Color(255, 170, 170),
+            sf::Color(212, 106, 106),
+            sf::Color(170, 57, 57),
+            sf::Color(128, 21, 21),
+            sf::Color(85, 0, 0),
+    };
+
     sf::Color openCellColor(34, 102, 102);
     sf::Color closedCellColor(170, 57, 57);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution distr(0, 1);
 
     while (window.isOpen())
     {
@@ -25,8 +48,8 @@ int main()
 
         window.clear(sf::Color::White);
 
-        int cellXCount = 40;
-        int cellYCount = 40;
+        int cellXCount = 80;
+        int cellYCount = 80;
 
         float cellXSize = window.getSize().x / cellXCount;
         float cellYSize = window.getSize().y / cellYCount;
@@ -39,17 +62,11 @@ int main()
                 sf::RectangleShape outerCell(cellSize);
                 outerCell.setPosition(sf::Vector2f(cellXSize * ix, cellYSize * jx));
 
-                if ((ix + jx) % 2 == 0)
-                {
-                    outerCell.setFillColor(closedCellColor);
-                }
-                else
-                {
-                    outerCell.setFillColor(openCellColor);
-                }
+                int c = abs((int)((sin(ix) + sin(jx)) * 256));
 
+                outerCell.setFillColor(sf::Color(c, c, c));
                 outerCell.setOutlineColor(borderColor);
-                outerCell.setOutlineThickness(2.f);
+                outerCell.setOutlineThickness(1.f);
 
                 window.draw(outerCell);
             }
